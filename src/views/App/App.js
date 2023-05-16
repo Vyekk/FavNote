d97098bf3da9e4146d73ed82d2737233f0bcaeb9
Vyekk <s20699@pjwstk.edu.pl>
2023-05-16 22:34:19 +0200

    Prepare files to new project

---
+++ src/views/App/App.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: aquamarine;
+`;
+
+const App = () => (
+  <div>
+    <h1>Hello world!</h1>
+    <Button>Click me!</Button>
+  </div>
+);
+
+export default App;
